d39ffa8c771b848d4931f30d0ce33e10eb7ed8ce
Ben Schmidt <bmschmidt@gmail.com>
2019-11-04 12:09:24 -0500

    Timeline version

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React, { Component } from 'react';
 import './App.css';
 import Bookworm from 'bookworm-vega';
 import Form from "react-jsonschema-form";
+import { get, set } from 'lodash-es';
 
 class App extends Component {
 
 constructor(props) {    
     super(props)
-    const bookworm = new Bookworm("#canvas", "http://localhost:10012", 600, 400)
+    const bookworm = new Bookworm("#canvas", "http://localhost:10012", window.innerWidth * .66, 400)
           try {
              bookworm.query = JSON.parse(decodeURI(window.location.hash.slice(1)))        
           } catch (e) {
@@ -19,24 +20,17 @@ constructor(props) {
             }
           }
     this.state = { bookworm: bookworm }
-    bookworm.plotAPI(bookworm.query)
   }
 
   componentDidMount() {
     this.state.bookworm.querySchema()
       .then(d => { this.setState({schema: d}) })
+    this.state.bookworm.plotAPI(this.state.bookworm.query)
   }
 
   render() {
-    const onSubmit = ({formData}, e) => {
-      this.setState(state => {
-	     state.bookworm.query.database = formData
-	     return state
-      })
-    }
-    
-      
-    const styles = {}//"transform": "scale(0.9, 0.9)"}
+ 
+    const styles = {"transform": "scale(0.7, 0.7)"}
 
     return <div className="App container">
        <div className="row">
@@ -44,56 +38,125 @@ constructor(props) {
          <h2> Bookworm Explorer Demonstration </h2>
          This is a demonstration of the redone Bookworm charting and 
          querying logic for debugging, developer exploration, and preliminary sharing.
-        It is bundled into a react Application: the 
-         bottom uses the react-jsonschema-form
-         library to make the full JSON schema for the Bookworm query interactive.
-         (With some exceptions; I've left out a few elements that the 
-         rendering library doesn't support.)
-         this is not a sensible end-user UX, but it should provide a general 
-         framework for showing how something like the old linechart explorer
-         could be refit to a modern web environment. The right side shows the rendered
-         charts; several different chart types (heatmap, streamgraph, barchart, etc.)
-         are currently supported. They are all a rather light wrapper around
-         vega-lite marks, with the data typing handled automatically based
-         on the Boookworm-defined properties.
        </div>
      </div>
      <div className="row">
       <div className ="col-md-12"> <Chart /> </div>
      </div>
          
-       <div className = "col-md-12" style={styles}>
-       <button onClick={()=>{ alert(JSON.stringify(this.state.bookworm.query)); }}>Show Query</button>
-                                                                                                                                                                                                                                  <BookwormForm bookworm={this.state.bookworm} database={this.state.bookworm.query.database}/>
-       <Form schema={{"description": "Change the Bookworm database being queried.", "type":"string", "default": this.state.bookworm.query.database || "federalist_bookworm"}}
-        onChange = {()=>{}}
-        onSubmit = {onSubmit}
-        onError  = {({formData}, e) => {console.log("Error", e)} }/>
-       </div>
-
+      <div className = "col-md-12 row" style={styles}>
+      <button onClick={()=>{ alert(JSON.stringify(this.state.bookworm.query)); }}>Show Query</button>
+      <div className = "col-md-4">
+      <BookwormCustom APIkey = "plottype" bookworm={this.state.bookworm} />
+      </div>
+      <div className = "col-md-6">
+      <BookwormWords bookworm={this.state.bookworm} />
+      </div>
+      </div>
       </div>
   }
 }
 
-/* Needs to re-render on changes to props.bookworm.query, which it doesn't currently see
-
-class QueryField extends Component {
+class BookwormCustom extends Component {
+  
   constructor(props) {
     super(props)
+    this.state = {schema: {}, key: props.APIkey}
   }
   
+  componentDidMount() {
+    this.props.bookworm.querySchema().then(
+      fullSchema => {
+	console.log(fullSchema)
+	let schema = get(fullSchema.properties, this.state.key)
+	if (schema) {
+	  schema.definitions = fullSchema.definitions;
+	} else {
+	  schema = {"type": "array", "items": {"type": "string"}}
+	}
+	
+        this.setState({schema: schema})
+      }
+    )
+  }
+
+  
   render() {
-    return <textarea rows="15" cols="100">{JSON.stringify(this.props.bookworm.query, undefined, 2)}
-    </textarea>
+    const onSubmit = ({formData}, e) => {
+      const query = JSON.parse(JSON.stringify(this.props.bookworm.query))
+      set(query, this.state.key, formData)
+      window.location.hash = encodeURI(JSON.stringify(query))
+      this.props.bookworm.plotAPI(query)
+  }
+ 
+    return <Form schema={this.state.schema}
+    formData = {get(this.state.key, this.props.bookworm.query)}
+    
+     onSubmit = {onSubmit}
+     onError  = {({formData}, e) => {console.log(e)} }/>
+  }
+  
+}
+
+class BookwormWords extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {schema: {}};
+  }
+
+  componentDidMount() {
+    this.props.bookworm.querySchema().then(
+      d => {
+	const schema = {
+	  description: "words",
+	  type: "array",
+	  items: {"type": "string"}
+	}
+        this.setState({schema: schema})
+      }
+    )
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    
+    if (prevProps.database !== this.props.database) {
+      console.log("Rebuilding master UI Schema")
+      this.props.bookworm.querySchema().then(
+        d => {
+          this.setState({schema: d})
+        }
+      )
+    }
+  }
+
+  render() {
+    const onSubmit = ({formData}, e) => {
+      const query = this.props.bookworm.query
+      formData.forEach((word, i) => {
+	if (query.search_limits[i] === undefined) {
+	  query.search_limits[i] = JSON.parse(JSON.stringify(query.search_limits[0]))
+	}
+	query.search_limits[i].word = [word]
+      })
+      window.location.hash = encodeURI(JSON.stringify(query))
+      this.props.bookworm.plotAPI(query)
+    }
+    
+    return <Form schema={this.state.schema}
+    formData = {this.props.bookworm.query.search_limits.map(d => d.word[0])}
+   //  onChange = {console.log("State change registered")}
+     onSubmit = {onSubmit}
+     onError  = {({formData}, e) => {console.log(e)} }/>
   }
 }
-*/
+
 
 class BookwormForm extends Component {
 
   constructor(props) {
     super(props);
-    this.state = {schema: {}};
+    this.state = {schema: {}, key: props.APIkey};
   }
 
   componentDidMount() {
@@ -118,28 +181,42 @@ class BookwormForm extends Component {
 
   render() {
     const onSubmit = ({formData}, e) => {
+      
       window.location.hash = encodeURI(JSON.stringify(formData))
       this.props.bookworm.plotAPI(formData)
     }
+    
     const UISchema = { 
       "search_limits": {"ui:options": {"orderable": false}},
-      "smoothingSpan": {"ui:widget": "hidden"},
-      // Changing the database alters the schema itself.
-//      "database": {"ui:widget": "hidden"}
     }
+    if (this.state.schema.properties === undefined) {return null}
+
+    for (const k of Object.keys(this.state.schema.properties)) {
+      if (UISchema[k] === undefined) {
+	UISchema[k] = {}
+      }
+      UISchema[k]["ui:widget"] = (k === this.state.key) ? undefined : "hidden"
+    }
+
+    const schema = JSON.parse(JSON.stringify(this.state.schema.properties[this.state.key]))
+    
+    console.log(UISchema)
+    
     return <Form schema={this.state.schema}
      uiSchema = {UISchema}
      formData = {this.props.bookworm.query}
-     onChange = {console.log("State change registered")}
+   //  onChange = {console.log("State change registered")}
      onSubmit = {onSubmit}
      onError  = {({formData}, e) => {console.log(e)} }/>
   }
 }
 
 class Chart extends Component {
+
   render() {
     return <div id="canvas"></div>
   }
+  
 }
 
 export default App
